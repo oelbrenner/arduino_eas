5b820f732d5d0b6302808534e7880fab80f99842
Jason Fraley <whiskeytangoeng@gmail.com>
2021-02-20 20:46:59 -0500

    indicator slider and cleanup

--- rotary-encoders/encoder/encoder.ino
+++ rotary-encoders/encoder/encoder.ino
@@ -10,8 +10,6 @@
 
 // LED pin to act as stand in for mosfet
 const int mosfetLed_Pin = 13;
-
-
 // screen setup
 #include <SPI.h>
 #include "Adafruit_GFX.h"
@@ -39,7 +37,6 @@ Adafruit_HX8357 tft = Adafruit_HX8357(TFT_CS, TFT_DC, TFT_RST);
 #define YELLOW  0xFFE0
 #define WHITE   0xFFFF
 #define ORANGE  0xFD20
-
 // Global vars
 // number of encoders
 int numEnc = 4;
@@ -74,6 +71,9 @@ int rfSensor = 0;
 int lrSensor = 0;
 int rrSensor = 0;
 
+//current wheel
+const int current_wheel[4] = {0, 1, 2, 3};
+
 // height sensor pins
 const int lfSensorPin = 6;
 const int rfSensorPin = 7;
@@ -89,23 +89,17 @@ const int DiaphramValvePin = 14;
 const int OutletValvePin   = 15;
 const int InletValvePin    = 16;
 
-
 // individual wheel dwell time
 int singleDwell = 200;
 // all wheels dwell time
 int allDwell = 300;
 
-// setup all the vars for the LCD screen
-char lfPosString, rfPosString, lrPosString, rrPosString, msPosString;
-char line1[21];
-char line2[21];
-char line3[21];
-char line4[21];
-
 void setup () {
   Serial.begin(9600);
   // refactor this to blink LED for boot sequence
+  Serial.println("Booting..");
   delay(20);
+  Serial.println("Done");
   // start up the TFT screen
   tft.begin();
   // rotation 1 is landscape with SPI pins on right side
@@ -113,8 +107,12 @@ void setup () {
   blackScreen();
   //splashScreen();
   //blackScreen();
-  colorSegment();
 
+  // set height selection boxes
+  heightSelectBoxes(WHITE);
+  // set up the color bars
+  Serial.println("building color bars");
+  colorSegments();
 
   // pinMode(mosfetLed_Pin, OUTPUT);
   // digitalWrite(mosfetLed_Pin, solenoidActivity_State);
@@ -139,61 +137,124 @@ void setup () {
   // }
 }
 
-// void loop () {
-//
-//   currentMillis = millis();
-//   check_encoders();
-//
-// }
-
 void loop(void) {
-  cornerRects(WHITE);
+  Serial.println("read height sensors");
+  read_height_sensors();
+  Serial.println("---------");
+  Serial.println("Loop complete.");
 }
 
 // fill screen with black
 unsigned long blackScreen() {
-  unsigned long start = micros();
   tft.fillScreen(HX8357_BLACK);
-  return micros() - start;
 }
-// color bars
-unsigned long colorSegment() {
-  unsigned long start = micros();
-  // red segments
-  tft.fillRect(0,     0, 21, 10, RED); // LF
-  tft.fillRect(459,   0, 21, 10, RED); // RF
-  tft.fillRect(0,   200, 21, 10, RED); // LR
-  tft.fillRect(459, 200, 21, 10, RED); // RR
-  // yellow segments
-  tft.fillRect(0,    10, 21, 30, YELLOW); // LF
-  tft.fillRect(459,  10, 21, 30, YELLOW); // RF
-  tft.fillRect(0,   210, 21, 30, YELLOW); // LR
-  tft.fillRect(459, 210, 21, 30, YELLOW); // RR
-  // green segments
-  tft.fillRect(0,    40, 21, 50, GREEN); // LF
-  tft.fillRect(459,  40, 21, 50, GREEN); // RF
-  tft.fillRect(0,   240, 21, 50, GREEN); // LR
-  tft.fillRect(459, 240, 21, 50, GREEN); // RR
-  // orange segments
-  tft.fillRect(0,    90, 21, 30, ORANGE); // LF
-  tft.fillRect(459,  90, 21, 30, ORANGE); // RF
-  tft.fillRect(0,   290, 21, 30, ORANGE); // LR
-  tft.fillRect(459, 290, 21, 30, ORANGE); // RR
 
+unsigned long colorSegments() {
+  // position: left-right,up-down,width,height,color
+  // color segments: red, yellow, green orange
+  // indicator
+  //black outline: 0-113
+  //white fill: 1-114
+  LFcolorSegment();
+  RFcolorSegment();
+  LRcolorSegment();
+  LRindicator();
+  RRcolorSegment();
+}
+
+unsigned long testColorSegments() {
+  unsigned long start = micros();
   // test stuff
   tft.fillRect(200, 25, 21, 65, GREEN); // test
   tft.fillRect(200, 10, 21, 30, YELLOW); // LF
   tft.fillRect(200,  0, 21, 10, RED); // LF
   tft.fillRect(200, 84, 21, 30, ORANGE); // LF
-  tft.fillRect(210,  4, 21, 5, WHITE); // LF
-  tft.drawRect(209,  3, 22, 6, BLACK);
-  tft.fillRect(210, 24, 21, 5, WHITE); // LF
-  tft.drawRect(209, 23, 22, 6, BLACK);
-  tft.fillRect(210, 44, 21, 5, WHITE); // LF
-  tft.drawRect(209, 43, 22, 6, BLACK);
-  tft.fillRect(210, 84, 21, 5, WHITE); // LF
-  tft.drawRect(209, 83, 22, 6, BLACK);
+  // indicator
+  tft.drawRect(209,  0, 22, 6, BLACK);
+  tft.fillRect(210,  1, 21, 5, WHITE);
+  tft.drawRect(209, 109, 22, 6, BLACK);
+  tft.fillRect(210, 110, 21, 5, WHITE);
   return micros() - start;
+}
+
+unsigned long LFcolorSegment() {
+  // Left Front
+  tft.fillRect(0,  0, 21, 10, RED); // LF
+  tft.fillRect(0, 10, 21, 30, YELLOW); // LF
+  tft.fillRect(0, 40, 21, 50, GREEN); // LF
+  tft.fillRect(0, 90, 21, 30, ORANGE); // LF
+  // black out area under indicator
+  tft.fillRect(21, 1, 10, 118, BLACK); // LF
+}
+
+int LFindicator(int lfSensor) {
+  // indicator top:0,1  bottom 114,115
+  LFcolorSegment();
+  tft.drawRect(9,  lfSensor,      22, 6, BLACK);
+  tft.fillRect(10, (lfSensor +1), 21, 5, WHITE);
+
+  //tft.drawRect(9,  114, 22, 6, BLACK);
+  //tft.fillRect(10, 115, 21, 5, WHITE);
+}
+
+unsigned long RFcolorSegment() {
+  // Right Front
+  tft.fillRect(459,   0, 21, 10, RED); // RF
+  tft.fillRect(459,  10, 21, 30, YELLOW); // RF
+  tft.fillRect(459,  40, 21, 50, GREEN); // RF
+  tft.fillRect(459,  90, 21, 30, ORANGE); // RF
+}
+
+unsigned long LRcolorSegment() {
+  // Left Rear
+  tft.fillRect(0,   200, 21, 10, RED); // LR
+  tft.fillRect(0,   210, 21, 30, YELLOW); // LR
+  tft.fillRect(0,   240, 21, 50, GREEN); // LR
+  tft.fillRect(0,   290, 21, 30, ORANGE); // LR
+}
+
+unsigned long LRindicator() {
+  // indicator top:200,201  bottom 314,315
+  tft.drawRect(9,  200, 22, 6, BLACK);
+  tft.fillRect(10, 201, 21, 5, WHITE);
+  tft.drawRect(9,  314, 22, 6, BLACK);
+  tft.fillRect(10, 315, 21, 5, WHITE);
+}
+
+unsigned long RRcolorSegment() {
+  // Right Rear
+  tft.fillRect(459, 200, 21, 10, RED); // RR
+  tft.fillRect(459, 210, 21, 30, YELLOW); // RR
+  tft.fillRect(459, 240, 21, 50, GREEN); // RR
+  tft.fillRect(459, 290, 21, 30, ORANGE); // RR
+}
+
+int read_height_sensors() {
+  Serial.print("-- reading height sensors --");
+  Serial.println();
+  // analog reads from the height sensors
+  // then map to the range of the display 
+  lfSensor = map(analogRead(lfSensorPin), 0, 1023, 114, 0);
+  //Serial.println("Left Front Height: ");
+  //Serial.println(lfSensor);
+  LFindicator(lfSensor);
+  // map: (pot value low, pot value high, range start, range end)
+  //current_wheel = "lf"
+  //update_height(lfsensor);
+
+  //rfSensor = analogRead(rfSensorPin);
+  //Serial.println("Right Front Height: ");
+  //Serial.println(rfSensor);
+
+  //lrSensor = analogRead(lrSensorPin);
+  //Serial.println("Left Rear Height: ");
+  //Serial.println(lrSensor);
+
+  //rrSensor = analogRead(rrSensorPin);
+  //Serial.println("Right Rear Height: ");
+  //Serial.println(rrSensor);
+  Serial.print("-- height sensor read complete --");
+  return lfSensor;
 }
 
 // startup message
@@ -217,7 +278,7 @@ unsigned long splashScreen() {
   return micros() - start;
 }
 
-unsigned long cornerRects(uint16_t color) {
+unsigned long heightSelectBoxes(uint16_t color) {
   unsigned long start = micros();
   // left front
   tft.drawRect(20, 0, 100, 120, color);
@@ -234,21 +295,34 @@ unsigned long cornerRects(uint16_t color) {
   tft.fillRect(364, 74, 92, 42, BLACK);// RF background
   tft.fillRect(24, 274, 92, 42, BLACK);// LR background
   tft.fillRect(364, 274, 92, 42, BLACK);// RR background
+ 
+  // put values in box
   tft.setTextColor(WHITE, BLACK);
+  tft.setTextSize(5);
+  tft.setCursor(36, 74);
+  tft.println("10");
+  tft.setCursor(376, 74);
+  tft.println("10");
+  tft.setCursor(36, 274);
+  tft.println("10");
+  tft.setCursor(376, 274);
+  tft.println("10");
 
-  for (int i = 0; i <= 42; i++) {
-    tft.setTextSize(5);
-    tft.setCursor(36, 74);
-    tft.println(i);
-    tft.setCursor(376, 74);
-    tft.println(i);
-    tft.setCursor(36, 274);
-    tft.println(i);
-    tft.setCursor(376, 274);
-    tft.println(i);
-    delay(60);
-  }
-  delay(200);
+  // test loop
+  //for (int i = 0; i <= 10; i++) {
+  //  tft.setTextSize(5);
+  //  tft.setCursor(36, 74);
+  //  tft.println(i);
+  //  tft.setCursor(376, 74);
+  //  tft.println(i);
+  //  tft.setCursor(36, 274);
+  //  tft.println(i);
+  //  tft.setCursor(376, 274);
+  //  tft.println(i);
+  //  delay(60);
+  //}
+
+  delay(100);
   return micros() - start;
 }
 
@@ -304,7 +378,7 @@ void check_encoders() {
       // if this encoder's position changed, flag the change variable
       if (encPos[c] != encPosLast[c]) {
         change = 1;
-        update_screen;
+        //update_screen;
       }
     }
   }
@@ -341,27 +415,6 @@ void decrement_encoder (int encoderNum, int encoderValue, int encoderValueLast) 
 
 }
 
-void read_height_sensors() {
-  Serial.print("entering read_height_sensors");
-  Serial.println();
-  // analog reads from the height sensors
-
-  lfSensor = analogRead(lfSensorPin);
-  Serial.print("Left Front Height: ");
-  Serial.print(lfSensor);
-
-  rfSensor = analogRead(rfSensorPin);
-  Serial.print("Right Front Height: ");
-  Serial.print(rfSensor);
-
-  lrSensor = analogRead(lrSensorPin);
-  Serial.print("Left Rear Height: ");
-  Serial.print(lrSensor);
-
-  rrSensor = analogRead(rrSensorPin);
-  Serial.print("Right Rear Height: ");
-  Serial.print(rrSensor);
-}
 
 void raise_wheel(int wheelID) {
   Serial.print("Raising wheel: ");
@@ -424,44 +477,3 @@ void lower_all() {
   digitalWrite(lfValvePin, LOW);
   digitalWrite(OutletValvePin, LOW);
 }
-
-
-void update_screen() {
-
-    Serial.print("entering update_screen");
-    for (c = 0; c < numEnc; c++) {
-          encPosLast[c] = encPos[c];
-    }
-
-    lfPosString = (encPos[0]);
-    rfPosString = (encPos[1]);
-    sprintf(line1, "%d %03d FRNT %d %03d", lfSensor, lfPosString, rfSensor, rfPosString);
-    //mySerial.write(254);
-    //mySerial.write(128);                     // move cursor to 1st position first line
-    //mySerial.write(line1);
-
-    lrPosString = (encPos[2]);
-    rrPosString = (encPos[3]);
-    sprintf(line2, "%d %03d REAR %d %03d", lrSensor, lrPosString, rrSensor, rrPosString);
-    //mySerial.write(254);
-    //mySerial.write(192);                     // move cursor to beginning of second line
-    //mySerial.write(line2);
-
-    msPosString = (encPos[4]);
-    sprintf(line3, "MASTER: %03d", msPosString);
-    //mySerial.write(254);
-    //mySerial.write(148);                     // move cursor to beginning of third line
-    //mySerial.write(line3);
-    //
-    //mySerial.write(254);
-    //mySerial.write(212);                     // move cursor to beginning of fourth line
-    //mySerial.write("____________________");
-
-    Serial.print(line1);
-    Serial.println();
-    Serial.print(line2);
-    Serial.println();
-    Serial.print(line3);
-    Serial.println();
-}
-
